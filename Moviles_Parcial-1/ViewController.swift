aa774377ae850b2cb76b4e89613dd7f039c85ae7
Alumno <itonygame@gmail.com>
2022-09-08 12:07:29 -0700

    Initial Commit

---
+++ Moviles_Parcial-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moviles_Parcial-1
+//
+//  Created by Alumno on 08/09/22.
+//  Copyright © 2022 César Escobedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
